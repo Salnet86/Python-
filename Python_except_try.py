76e807bbac6c40f9f3909693f47de4e5ace822ed
Tecnologie <dosal86@libero.it>
2024-09-05 09:21:27 +0200

    Add files via upload

---
+++ Python_except_try.py
@@ -0,0 +1,8 @@
+
+
+try:
+    Input_x=float(input("Insersci un numero ma non una striga di parole"))
+    print(f"Hai selezionato il valore {Input_x}")
+ 
+except:
+  print("Errore del programma l'input che hai selezionato e una striga devi selezionare solo numeri float") 
