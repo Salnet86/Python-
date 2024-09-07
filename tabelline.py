02a176c108a2a3b61faf329cfe16bbe28cb7db0e
Tecnologie <dosal86@libero.it>
2024-09-07 18:07:30 +0200

    Add files via upload

---
+++ tabelline.py
@@ -0,0 +1,12 @@
+print("Tabelline")
+n=11
+try:
+   
+    tab=int(input("Insersci tabellina"))
+    for i in range(n):
+        val=tab * i
+        print(f"La tabellina del {tab}  e uguale {val}")
+        
+        
+except:
+    print("Errore programma")
