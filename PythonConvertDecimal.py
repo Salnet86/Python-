a50093ee3e9ef59d8798e37605e60a5cd731f992
Tecnologie <dosal86@libero.it>
2024-09-11 18:30:45 +0200

    Python Convert Decimali
    Converti decimali numeri con virgola

---
+++ PythonConvertDecimal.py
@@ -0,0 +1,29 @@
+string='''
+
+       FORMATTA DECIMALI CON LA VIRGOLA
+
+prezzo1=13.14159
+x=223.786
+sposto la virgola in  due posti  13
+n="{:2.0f}".format(prezzo1)
+x=223.786
+
+Number=f"Numero {x:3.0f}"
+print(Number)
+
+'''
+
+print(string)
+
+
+x=223.786
+Number=f"Numero {x:3.0f}"
+print(Number)
+
+
+
+prezzo1=13.14159
+x=223.786
+n="{:2.0f}".format(prezzo1)
+print(n)
+
