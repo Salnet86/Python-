6b13ecb4dfd8362a94bcae52053042db8b194473
Tecnologie <dosal86@libero.it>
2025-05-13 21:31:32 +0200

    Create DoppioOutput.py

---
+++ DoppioOutput.py
@@ -0,0 +1,8 @@
+def somma_e_sottrazione(a, b):
+    risultati = [a + b, a - b]
+    return risultati
+
+# Esempio di utilizzo
+output = somma_e_sottrazione(10, 3)
+print("Somma:", output[0])
+print("Sottrazione:", output[1])
