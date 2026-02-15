3c5623e301efa0a58fa71f2f5014a7f19c134cea
Tecnologie <dosal86@libero.it>
2026-02-15 20:15:48 +0100

    Add files via upload

--- Piramide.py
+++ Piramide.py
@@ -1,22 +1,12 @@
-BACKGROUND_GREEN = '\033[42m'
-BLACK = '\033[30m'
 
-
-piramide='''
-         ---------------------------
-         |     Piramide Egiziana   |
-         ---------------------------
-'''
-
-print(BACKGROUND_GREEN,BLACK, piramide,end="")
-
-n =20
-for i in range(n):
+n=10
+for i in range(n) :
     for j in range(n-i):
-        print(" ", end="")
-    for k in range(2*i-1):
-        print("*",end="")
-         
-         
-       
+        
+        print(' ', end="" )
+    for k in range(2 * i + 1):
+        print(j , end='')
+            
     print()
+        # Questo "if" garantisce che lavori solo sul triangolo inferiore
+        # ... esegui eliminazione ...
